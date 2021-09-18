196e3276c1c88d2bd90e5c04cb7aa3b4c60f82a6
Hani al Sabti <h.sabti87@gmail.com>
2021-09-18 21:03:23 +0200

    added checkout post method

--- api/checkout.js
+++ api/checkout.js
@@ -1,10 +1,10 @@
-const stripeAPI = require("./stripe");
+const stripeAPI = require("../stripe");
 
 const createCheckoutSession = async (req, res) => {
   const domainUrl = process.env.WEB_APP_URL;
   const { line_items, customer_email } = req.body;
 
-  if (!line_items || customer_email) {
+  if (!line_items || !customer_email) {
     return res
       .status(400)
       .json({ error: "missing required session parameters" });
@@ -13,16 +13,16 @@ const createCheckoutSession = async (req, res) => {
   let session;
 
   try {
-    session = await stripeAPI.checkout.session.create({
+    session = await stripeAPI.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
       line_items,
       customer_email,
-      success_url: `${domainUrl}/success_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${domainUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${domainUrl}/canceled`,
       shipping_address_collection: { allowed_countries: ["NL", "GB", "US"] },
     });
-    res.status(200).json({ sessionID: session.id });
+    res.status(200).json({ sessionId: session.id });
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: "unable to create a session" });
