8aa157e82e2b8d0822732ad30e0800880f8016e6
Hani al Sabti <h.sabti87@gmail.com>
2021-09-21 18:31:01 +0200

    added env port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: "./.env" });
 const createCheckoutSession = require("./api//checkout");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors({ origin: true }));
